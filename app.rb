617511f68acfaa29347f668eaa574006abff9384
Alexander Urizar <urizaraj@gmail.com>
2018-02-28 19:51:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -36,7 +36,7 @@ class App < Sinatra::Base
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
 
-    case operation
+    case @operation
     when 'add'
       return "#{@num1 + @num2}"
     when 'subtract'
