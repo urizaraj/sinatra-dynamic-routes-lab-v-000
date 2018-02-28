ee63de1b38e8d17cdb9ee18ef352d6d8d726c8a9
Alexander Urizar <urizaraj@gmail.com>
2018-02-28 19:48:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -30,4 +30,10 @@ class App < Sinatra::Base
 
     "#{words.join(' ')}."
   end
+
+  get '/:operation/:number1/:number2' do
+    @operation = params[:operation]
+    @num1 = params[:number1]
+    @num2 = params[:number2]
+  end
 end
